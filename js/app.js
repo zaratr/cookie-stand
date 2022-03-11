bed2dc2c394af8631e36a4e364fb0036e043ff8a
raul <zarater.raul@gmail.com>
2022-03-11 15:08:06 -0800

    dkfs

---
+++ js/app.js
@@ -0,0 +1,58 @@
+'use strict'
+
+
+class storeMinMax {
+    hourlyCustomers;
+    averageCookies;
+    random;
+    constructor() {
+        this.hourlyCustomers = 0;
+        this.averageCookies = 0;
+        this.random = Math.floor(Math.random() * (10 - 0) + 0);
+    }
+}
+class storeInit1 extends storeMinMax {
+
+    constructor(clientHour, clientAverage) {
+        super();
+        if(clientAverage > 0 && !clientHour)
+        this.hourlyCustomers = clientHour;
+        else if(clientHour > 0 && !clientAverage)
+        this.clientAverage = clientAverage;
+        else if(clientAverage, clientHour)
+        {
+            this.averageCookies = clientAverage;
+            this.hourlyCustomers = clientHour;
+        }
+    }
+}
+
+class storeInit2 extends storeMinMax
+{
+    constructor(toCpyObj)
+    {
+        super();
+        if(toCpyObj)//if it exists
+        {
+            this.averageCookies = toCpyObj.averageCookies;
+            this.hourlyCustomers = toCpyObj.hourlyCustomers;
+        }
+        //else //(!tocpyObj) - we dont need to do anything because obj doesn't exist for copy
+        //return; //TODO: try using try catches..
+    }
+}
+
+function main()//works similar to main in other programming languages. for simplicity all work will be here.
+{
+    let obj1 = new storeInit1(1,2);
+    let obj2 = new storeInit2(obj1);
+
+    return 0;
+
+}
+
+main();
+
+
+
+
