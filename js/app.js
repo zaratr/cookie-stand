7e35d65544c661d4a7156e2c70345d913a42f809
raul <zarater.raul@gmail.com>
2022-03-12 13:53:53 -0800

    initialized some code . now i'm on question 4

--- js/app.js
+++ js/app.js
@@ -1,54 +1,152 @@
 'use strict'
 
 
-class storeMinMax {
-    hourlyCustomers;
-    averageCookies;
-    random;
-    constructor() {
-        this.hourlyCustomers = 0;//use random here
-        this.averageCookies = 0;//use this with random
-        this.random = Math.floor(Math.random() * (65 - 23) + 23);// for every hour
+let hoursAndAverages = {
+    timeArr : [],
+    CalcAverage : [],
+    toSetAverage : [],
+    hourlyCustomers : [] ,
+    averageCookies : [], //Math.floor(math.random() * (10 - 0) + 0);
+
+    setTime: function(min, max)
+    {
+        let len = max - min;
+        this.timeArr[len];
+        for(let i = 0; i < len; ++i)
+        {
+            this.timeArr[i] = min;
+            ++min;
+        }
+        return this.timeArr;
+    },
+
+    setAverage: function(min, max, cookiesOrCustomer)
+    {
+        this.setTime(6,20);//sets time for fixed hours of day
+        let len = this.timeArr.length;
+        let randomBuff;
+        this.CalcAverage[len]
+        this.toSetAverage[2];
+        for(let i = 0; i < len; ++i)
+        {
+            for(let j = 0; j < 2; ++j)
+            { 
+                randomBuff = this.randoFunc(min, max)
+                //tuple array TODO: map
+                this.toSetAverage[j] = randomBuff;
+            }
+            //main buffer array for setting array with average
+            this.CalcAverage[i] = this.toSetAverage;
+
+        }
+        if(cookiesOrCustomer === "cookies")
+        {
+            //sets cookie average
+            //this.averageCookies[len];
+            this.averageCookies = this.CalcAverage;
+            return this.averageCookies;
+        }
+        else //!== "cookies  instead the string customers is expected here" 
+        {
+            //sets customer average
+            this.hourlyCustomers[len];
+            for(let i = 0; i < len; ++i)
+            this.hourlyCustomers[i] = this.CalcAverage[i][0];
+
+            return this.hourlyCustomers;
+        }
+
+    },
+    randoFunc: function(min, max)
+    {
+        let randomBuff = 0 ;
+        let arr = []
+        for(let i = 0; i < 5; ++i)
+        {
+
+            //TODO: 1. don't need array. 2. this needs time.sleep() because it gives same number
+
+            arr[i] = parseInt(Math.random() * (max - min) + min);// ALERT: for every hour. doesn't need a floor because in return statemetn
+            randomBuff += arr[i];
+        }
+
+        return Math.floor(randomBuff /= 5);
     }
 }
-class storeInit1 extends storeMinMax {
+//sets all to default
+let storeObj = {
+    timer : hoursAndAverages.setTime(6, 20),
+    hourlyCustomers : hoursAndAverages.setAverage(0, 10, "customers"),
+    averageCookies : hoursAndAverages.setAverage(23, 65, "cookies"),
+/*
+    setValues: function() {
+        this.timer = hoursAndAverages.setTime(min, max);
+    }
+*/
+}
 
-    constructor(clientHour, clientAverage) {
-        super();
-        if(clientAverage > 0 && !clientHour)
-        this.hourlyCustomers = clientHour;
-        else if(clientHour > 0 && !clientAverage)
-        this.clientAverage = clientAverage;
-        else if(clientAverage, clientHour)
-        {
+//sets values passed in by argument. similar to constructor
+let storeInit1 = {
+    hourlyCustomers : 0,
+    clientAverage : 0,
+    timer : hoursAndAverages.setTime(6,20),
+
+    setValues: function(clientHour, clientAverage) 
+    {
+        this.timer = hoursAndAverages.setTime(min, max);
+        if (clientAverage > 0 && !clientHour) {
+            this.hourlyCustomers = clientHour;
+        }
+        else if (clientHour > 0 && !clientAverage) {
+            this.clientAverage = clientAverage;
+        }
+        else if (clientAverage, clientHour) {
             this.averageCookies = clientAverage;
             this.hourlyCustomers = clientHour;
         }
     }
-}
+/*
+*/
+};
+/*
+*/
+//copies an object similar parameters. although now i know we can just add into it
+let storeInit2 = {
+    averageCookies : 0,
+    hourlyCustomers : 0,
+    timer : 0,
 
-class storeInit2 extends storeMinMax
-{
-    constructor(toCpyObj)
-    {
-        super();
+    setValues : function(toCpyObj){
         if(toCpyObj)//if it exists
         {
+            this.timer = toCpyObj.timer;
             this.averageCookies = toCpyObj.averageCookies;
             this.hourlyCustomers = toCpyObj.hourlyCustomers;
         }
-        //else //(!tocpyObj) - we dont need to do anything because obj doesn't exist for copy
-        //return; //TODO: try using try catches..
+        return;
     }
-}
+};
 
 function main()//works similar to main in other programming languages. for simplicity all work will be here.
 {
-    let obj1 = new storeInit1(1,2);
-    let obj2 = new storeInit2(obj1);
+    let aCompany = storeObj;
+    //obj.hoursAndAverages.setAverage(0, 10, "customer");
+    let costumersPurchased = aCompany.hourlyCustomers;
+    let hoursOfOperation = aCompany.timer;
+    let cookiesPurchased = aCompany.averageCookies;
+
+    /*array that hass all the values needed. 
+    all are arrays except cookiesPurchased. 
+    cookiesPurchase is a 2d Array/map*/
+    const storeArr = [
+        "Seattle", `${hoursOfOperation}`, `${cookiesPurchased}`, `${costumersPurchased}`,
+        "Tokyo", `${hoursOfOperation}`, `${cookiesPurchased}`, `${costumersPurchased}`,
+        "Dubai", `${hoursOfOperation}`, `${cookiesPurchased}`, `${costumersPurchased}`,
+        "Paris", `${hoursOfOperation}`, `${cookiesPurchased}`, `${costumersPurchased}`,
+        "Lima", `${hoursOfOperation}`, `${cookiesPurchased}`, `${costumersPurchased}`
+    ];
 
     return 0;
-
 }
 
 main();
