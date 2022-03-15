f1c67e053daf03e3cc5de12da72f21f19bc28eb5
raul <zarater.raul@gmail.com>
2022-03-15 16:06:00 -0700

    init using the constructor. will still need to create a table

--- js/app.js
+++ js/app.js
@@ -1,311 +1,96 @@
-'use strict'
-
 
 let hours = [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];//this is from 6am - 8pm
 
-let seattle = {
-        name: "Seattle",
-        min: 23,
-        max: 65,
-        avg: 6.3,
+function random(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-        customers : [],
-        cookies : [],//average per hour
-        trackerTotalCookies: 0,//total cookies
-        setCustomers: function()
-        {
-            for(let i of hours)
-            {
-                //loops through array 
-                this.customers.push(Math.floor(Math.random() * (this.max - this.min + 1) + this.min));
-            }
-        },
+//array to hold constructors. constructors are written in functions in js
+let citiesObj = [];//will be filled later.
 
-        setCookies: function()
-        {
+//constructor implementation
+function City(name, avg, min, max)
+{
+    this.name = name;
+    this.max = max;
+    this.min = min;
+    this.avg = avg;
 
-            this.setCustomers();//calls this to init
-            let len = this.customers.length;
-            //must be set before using 
-            //do a loop to init 
-            for(let i = 0; i < len; ++i)
-            {
-                const hour = Math.ceil(this.customers[i] * this.avg);
-                this.cookies.push(hour);
-                this.trackerTotalCookies += hour;
-            }
+    this.costumers = [];
+    this.cookies = [];
+    this.trackerTotalCookies = 0;
 
-        },
+    //init the constructore here js way
+    citiesObj.push(this);
+    return; 
+}
 
-        renderer: function(anhtmlElement)
-        {
-            //sets to the dom
-            //renders to html
-            this.setCookies();//sets all 
-            let len = this.cookies.length;
-            document.getElementById("sName").innerHTML = this.name;//title for ul 
-            let list = document.getElementById(anhtmlElement) 
-            for(let i = 0; i < len; ++i)
-            {
-                //create by tag
-                let listItem = document.createElement('li');
-                listItem.textContent=hours[i]  + " : " + this.cookies[i] + " Cookies";
-                //append to list
-                list.appendChild(listItem);
-            }
-            let finalListItem = document.createElement('li');
-            finalListItem.textContent = "total: " + this.trackerTotalCookies;
-            list.appendChild(finalListItem);
+//method implementation is done out of function using prototype keyword.
+City.prototype.setCustomers = function()
+{
+    for(let i of hours)
+    {
+        this.costumers.push(random(this.min, this.max));
+    }
+}
+City.prototype.setCookies = function()
+{
+    let len = this.costumers.length;
+    for(let i = 0; i < len; ++i)
+    {
+        const hour = Math.ceil(this.costumers[i] * this.avg);
+        this.cookies.push(hour);
+        this.trackerTotalCookies += hour;
+    }
+}
 
-        }
+City.prototype.render = function()
+{
+//user defined parameter that is a string
+    let userIdName;
+    switch(this.name)
+    {
+        case "Seattle" : userIdName = "sName"; break;
+        case "Tokyo"    : userIdName = "tName"; break;
+        case "Dubai"    : userIdName = "dName"; break;
+        case "Paris"    : userIdName = "pName"; break;
+        case "Lima"     : userIdName = "lName"; break;
     }
 
-let tokyo = {
-        name: "Tokyo",
-        min: 3,
-        max: 24,
-        avg: 1.2,
-
-        customers : [],
-        cookies : [],//average per hour
-        trackerTotalCookies: 0,//total cookies
-        setCustomers: function()
-        {
-            for(let i of hours)
-            {
-                //loops through array 
-                this.customers.push(Math.floor(Math.random() * (this.max - this.min + 1) + this.min));
-            }
-        },
-
-        setCookies: function()
-        {
-
-            this.setCustomers();//calls this to init
-            let len = this.customers.length;
-            //must be set before using 
-            //do a loop to init 
-            for(let i = 0; i < len; ++i)
-            {
-                const hour = Math.ceil(this.customers[i] * this.avg);
-                this.cookies.push(hour);
-                this.trackerTotalCookies += hour;
-            }
-
-        },
-
-        renderer: function(anhtmlElement)
-        {
-            //sets to the dom
-            //renders to html
-            this.setCookies();//sets all 
-            let len = this.cookies.length;
-            document.getElementById("tName").innerHTML = this.name;//title for ul 
-            let list = document.getElementById(anhtmlElement);
-            for(let i = 0; i < len; ++i)
-            {
-                //create
-                //by tag
-                let listItem = document.createElement('li');
-                //
-                listItem.textContent=hours[i] + " : " + this.cookies[i] + " Cookies";
-                //append to list
-                list.appendChild(listItem);
-            }
-            let finalListItem = document.createElement('li');
-            finalListItem.textContent = "total: " + this.trackerTotalCookies;
-            list.appendChild(finalListItem);
-
-        }
+    //renders to html
+    let len = this.cookies.length;
+    document.getElementById(userIdName).textContent = this.name;//title for ul 
+    let list = document.getElementById(this.name); 
+    for(let i = 0; i < len; ++i)
+    {
+        //create by tag
+        let listItem = document.createElement('li');
+        listItem.textContent=hours[i]  + " : " + this.cookies[i] + " Cookies";
+        //append to list
+        list.appendChild(listItem);
     }
+    let finalListItem = document.createElement('li');
+    finalListItem.textContent = "total: " + this.trackerTotalCookies;
+    list.appendChild(finalListItem);
+}
 
-let dubai = {
-        name: "Dubai",
-        min: 11,
-        max: 38,
-        avg: 3.7,
-
-        customers : [],
-        cookies : [],//average per hour
-        trackerTotalCookies: 0,//total cookies
-        setCustomers: function()
-        {
-            for(let i of hours)
-            {
-                //loops through array 
-                this.customers.push(Math.floor(Math.random() * (this.max - this.min + 1) + this.min));
-            }
-        },
-
-        setCookies: function()
-        {
-
-            this.setCustomers();//calls this to init
-            let len = this.customers.length;
-            document.getElementById("dName").innerHTML = this.name;//title for ul 
-            //must be set before using 
-            //do a loop to init 
-            for(let i = 0; i < len; ++i)
-            {
-                const hour = Math.ceil(this.customers[i] * this.avg);
-                this.cookies.push(hour);
-                this.trackerTotalCookies += hour;
-            }
-
-        },
-
-        renderer: function(anhtmlElement)
-        {
-            //sets to the dom
-            //renders to html
-            this.setCookies();//sets all 
-            document.getElementById("dName").innerHTML = this.name;//title for ul 
-            let len = this.cookies.length;
-            let list = document.getElementById(anhtmlElement);
-            for(let i = 0; i < len; ++i)
-            {
-                //create
-                //by tag
-                let listItem = document.createElement('li');
-                //
-                listItem.textContent=hours[i] + " : " + this.cookies[i] +"Cookies";
-                //append to list
-                list.appendChild(listItem);
-            }
-            let finalListItem = document.createElement('li');
-            finalListItem.textContent = "total: " + this.trackerTotalCookies;
-            list.appendChild(finalListItem);
-
-        }
-    }
-
-let paris = {
-        name: "Paris",
-        min: 20,
-        max: 38,
-        avg: 2.3,
-
-        customers : [],
-        cookies : [],//average per hour
-        trackerTotalCookies: 0,//total cookies
-        setCustomers: function()
-        {
-            for(let i of hours)
-            {
-                //loops through array 
-                this.customers.push(Math.floor(Math.random() * (this.max - this.min + 1) + this.min));
-            }
-        },
-
-        setCookies: function()
-        {
-
-            this.setCustomers();//calls this to init
-            let len = this.customers.length;
-            //must be set before using 
-            //do a loop to init 
-            for(let i = 0; i < len; ++i)
-            {
-                const hour = Math.ceil(this.customers[i] * this.avg);
-                this.cookies.push(hour);
-                this.trackerTotalCookies += hour;
-            }
-
-        },
-
-        renderer: function(anhtmlElement)
-        {
-            //sets to the dom
-            //renders to html
-            this.setCookies();//sets all 
-            document.getElementById("pName").innerHTML = this.name;//title for ul 
-            let len = this.cookies.length;
-            let list = document.getElementById(anhtmlElement);
-            for(let i = 0; i < len; ++i)
-            {
-                //create
-                //by tag
-                let listItem = document.createElement('li');
-                //
-                listItem.textContent=hours[i] + " : " + this.cookies[i] +"Cookies";
-                //append to list
-                list.appendChild(listItem);
-            }
-            let finalListItem = document.createElement('li');
-            finalListItem.textContent = "total: " + this.trackerTotalCookies;
-            list.appendChild(finalListItem);
-
-        }
-    }
-
-let lima = {
-        name: "Lima",
-        min: 2,
-        max: 16,
-        avg: 4.6,
-
-        customers : [],
-        cookies : [],//average per hour
-        trackerTotalCookies: 0,//total cookies
-        setCustomers: function()
-        {
-            for(let i of hours)
-            {
-                //loops through array 
-                this.customers.push(Math.floor(Math.random() * (this.max - this.min + 1) + this.min));
-            }
-        },
-
-        setCookies: function()
-        {
-
-            this.setCustomers();//calls this to init
-            let len = this.customers.length;
-            //must be set before using 
-            //do a loop to init 
-            for(let i = 0; i < len; ++i)
-            {
-                const hour = Math.ceil(this.customers[i] * this.avg);
-                this.cookies.push(hour);
-                this.trackerTotalCookies += hour;
-            }
-
-        },
-
-        renderer: function(anhtmlElement)
-        {
-            //sets to the dom
-            //renders to html
-            this.setCookies();//sets all 
-            document.getElementById("lName").innerHTML = this.name;//title for ul 
-            let len = this.cookies.length;
-            let list = document.getElementById(anhtmlElement);
-            for(let i = 0; i < len; ++i)
-            {
-                //create
-                //by tag
-                let listItem = document.createElement('li');
-                //
-                listItem.textContent=hours[i] + " : " + this.cookies[i] +"Cookies";
-                //append to list
-                list.appendChild(listItem);
-            }
-            let finalListItem = document.createElement('li');
-            finalListItem.textContent = "total: " + this.trackerTotalCookies;
-            list.appendChild(finalListItem);
-
-        }
-    }
+//Instantiate
+new City("Seattle", 23, 65, 6.3);
+new City("Tokyo", 3, 24, 1.2);
+new City("Dubai", 11, 38, 3.7);
+new City("Paris", 20, 38, 2.3);
+new City("Lima", 2, 16, 4.6);
 
 function main()
 {
-    seattle.renderer("seattle");
-    tokyo.renderer("tokyo");
-    dubai.renderer("dubai");
-    paris.renderer("paris");
-    lima.renderer("lima")
-    
-    return ;
+    for(city of citiesObj)
+    {
+        city.setCustomers();
+        city.setCookies();
+        city.render();
+    }
+    return;
 }
 
 main();
